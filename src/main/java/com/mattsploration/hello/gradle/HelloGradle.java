a6c00c6c74bba44f3a113cbbe2e63fc261f72b72
myattasaurus <rcoins84@yahoo.com>
2023-03-04 14:15:31 -0500

    Initial Commit

---
+++ src/main/java/com/mattsploration/hello/gradle/HelloGradle.java
@@ -0,0 +1,8 @@
+package com.mattsploration.hello.gradle;
+
+public class HelloGradle {
+
+    public static void main(String... args) {
+        System.out.println("Hello Gradle");
+    }
+}
---
+++ src/test/java/com/mattsploration/hello/gradle/HelloGradleTest.java
@@ -0,0 +1,11 @@
+package com.mattsploration.hello.gradle;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloGradleTest {
+
+    @Test
+    public void verifyNoExceptionThrown() {
+        HelloGradle.main();
+    }
+}
